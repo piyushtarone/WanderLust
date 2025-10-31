ed8ec03798d510a1a3b55a3fe7ec6ae3eb81d38e
Piyush Tarone <piyushtarone@gmail.com>
2025-10-31 21:53:52 +0530

    new Coludinary version 1

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const cloudinary = require("cloudinary").v2;
+const cloudinary = require("cloudinary");
 const {cloudinaryStorage, CloudinaryStorage} = require("multer-storage-cloudinary");
 
 cloudinary.config({
